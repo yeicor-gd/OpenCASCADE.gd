0bddb55b383a2f43670733f48df4a1791109e179
Yeicor <yeicor+no-reply@github.com>
2026-09-11 15:12:15 +0200

    fix(windows): set CMAKE_OBJECT_PATH_MAX 240, disable /MP under Ninja, and use /O1 /d2notmpopt on x86 MSVC

--- vcpkg_ports/gdext/portfile.cmake
+++ vcpkg_ports/gdext/portfile.cmake
@@ -1,5 +1,5 @@
 # Portfile for the Godot extension of this project
-# ABI change to force rebuild - v3
+# ABI change to force rebuild - v4
 
 # The dependencies are specified in vcpkg.json
 
--- vcpkg_triplets/x86-windows-static.cmake
+++ vcpkg_triplets/x86-windows-static.cmake
@@ -5,3 +5,7 @@ include("../vcpkg_triplets/common/windows-static.cmake")
 # Explicitly disable ICF (Identical Code Folding) for 32-bit static builds to prevent
 # memory exhaustion during linking.
 set(VCPKG_LINKER_FLAGS_RELEASE "/OPT:REF /OPT:NOICF /DEBUG:NONE")
+
+# Use /O1 and /d2notmpopt for 32-bit release builds to avoid MSVC C1001 optimizer internal compiler error
+set(VCPKG_CXX_FLAGS_RELEASE "/O1 /Oi /Gy /DNDEBUG /Z7 /d2notmpopt")
+set(VCPKG_C_FLAGS_RELEASE "/O1 /Oi /Gy /DNDEBUG /Z7 /d2notmpopt")
