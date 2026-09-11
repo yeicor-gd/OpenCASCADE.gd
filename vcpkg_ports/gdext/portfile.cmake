bc6b2db564e767e7341286cf047979d23344a6d8
Yeicor <yeicor+no-reply@github.com>
2026-09-11 18:53:50 +0200

    fix(windows): preserve GDEXT_CMAKE_ARGS in CI and drop unsupported /d2notmpopt MSVC flag

--- vcpkg_ports/gdext/portfile.cmake
+++ vcpkg_ports/gdext/portfile.cmake
@@ -1,5 +1,5 @@
 # Portfile for the Godot extension of this project
-# ABI change to force rebuild - v4
+# ABI change to force rebuild - v5
 
 # The dependencies are specified in vcpkg.json
 
--- vcpkg_triplets/x86-windows-static.cmake
+++ vcpkg_triplets/x86-windows-static.cmake
@@ -6,6 +6,6 @@ include("../vcpkg_triplets/common/windows-static.cmake")
 # memory exhaustion during linking.
 set(VCPKG_LINKER_FLAGS_RELEASE "/OPT:REF /OPT:NOICF /DEBUG:NONE")
 
-# Use /O1 and /d2notmpopt for 32-bit release builds to avoid MSVC C1001 optimizer internal compiler error
-set(VCPKG_CXX_FLAGS_RELEASE "/O1 /Oi /Gy /DNDEBUG /Z7 /d2notmpopt")
-set(VCPKG_C_FLAGS_RELEASE "/O1 /Oi /Gy /DNDEBUG /Z7 /d2notmpopt")
+# Use /O1 for 32-bit release builds to avoid MSVC C1001 optimizer internal compiler error
+set(VCPKG_CXX_FLAGS_RELEASE "/O1 /Oi /Gy /DNDEBUG /Z7")
+set(VCPKG_C_FLAGS_RELEASE "/O1 /Oi /Gy /DNDEBUG /Z7")
